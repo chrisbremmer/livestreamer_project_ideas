ae4f147211a684c03fdc2b1ec4b59b9fdcc29c48
chrisbremmer <c_bremmer@outlook.com>
2019-10-08 18:55:07 -0700

    Set up Idea service

--- app.js
+++ app.js
@@ -4,6 +4,29 @@ const socketio = require("@feathersjs/socketio");
 const moment = require("moment");
 
 // Idea Service
+class IdeaService {
+  constructor() {
+    this.ideas = [];
+  }
+
+  async find() {
+    return this.ideas;
+  }
+
+  async create(data) {
+    const idea = {
+      id: this.ideas.length,
+      text: data.text,
+      tech: data.tech,
+      viewer: data.viewer
+    };
+    idea.time = moment().format("h:mm:ss a");
+
+    this.ideas.push(idea);
+
+    return idea;
+  }
+}
 
 const app = express(feathers());
 
